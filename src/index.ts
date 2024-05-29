edcacee1e3b9acc09a7852cd22df783764c25691
Jessy Metaxas <jessy.metaxas@gmail.com>
2024-05-29 12:08:01 +0200

    clean pages

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-import { Picker } from './picker';
-customElements.define('color-picker', Picker);
--- src/types/module.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.css' {
-    const stylesheet: CSSStyleSheet;
-    export default stylesheet;
-}
-
-declare module '*?raw' {
-    const content: string;
-    export default content;
-}
